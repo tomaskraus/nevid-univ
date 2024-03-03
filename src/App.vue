9e5f99fff8d4f0357cdd58849ff0d9c8e0e3d023
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-03 17:07:09 +0100

    chore: first commit
    SheetItem component

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+
+      <v-container>
+        <v-row justify="space-around">
+          <v-col cols="12" md="6">
+            <SheetItem>Hello!</SheetItem>
+            <SheetItem>Hello2!</SheetItem>
+            <SheetItem>Hello3!</SheetItem>
+          </v-col>
+          <v-col cols="12" md="6">
+            <SheetItem>World</SheetItem>
+            <SheetItem></SheetItem>
+          </v-col>
+        </v-row>
+      </v-container>
+
+
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+
+
+const elevations = [0, 4, 8, 12, 16, 20]
+
+
+//
+</script>
---
+++ src/components/SheetItem.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-sheet class="pa-12 my-3" :elevation="2">
+
+    <slot>content</slot>
+  </v-sheet>
+</template>
