7fd57dea48571dd8c2ef563d6f66793725665194
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-03 21:50:05 +0100

    feat: conditional label, rouded top, bottom label border

--- src/components/SheetItem.vue
+++ src/components/SheetItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-sheet  :elevation="2" class="my-3 "  >
-    <v-sheet class="px-3 py-1 font-weight-bold" color="grey-lighten-2">{{label || '{Label}'}}:</v-sheet>
+  <v-sheet  :elevation="2" class="mb-4 mx-2">
+    <v-sheet v-if="label" class="px-3 py-1 font-weight-bold border-b border-darker rounded-t"  color="grey-lighten-2">{{label}}:</v-sheet>
     <v-sheet class="pa-3">
       <slot>{content}</slot>
     </v-sheet>
