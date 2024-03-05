6e3ae4fa6d4348c97bc697a36625bc648f77d61a
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-05 02:27:34 +0100

    chore(PairItem): width, margin bottom

--- src/components/PairItem.vue
+++ src/components/PairItem.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="pa-0 ma-0" style="max-width: 18em;">
-        <v-row class="pa-0 ma-0 mb-0">
+        <v-row align="center" class="pa-0 ma-0 mb-2">
           <v-col cols="6" class="font-weight-bold pa-0 ma-0">
             {{ label }}:
           </v-col>
-          <v-col cols="6" class="pa-0 ma-0">
+          <v-col cols="6" class="pa-0 ma-0 w-auto">
             <slot></slot>
           </v-col>
         </v-row>
