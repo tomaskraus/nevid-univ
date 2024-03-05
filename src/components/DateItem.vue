d896aa2661ee392f9edcca456d69b5ba45c32193
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-06 00:11:34 +0100

    feat(DateItem): safe for falsy values

--- src/components/DateItem.vue
+++ src/components/DateItem.vue
@@ -8,15 +8,18 @@ const props = defineProps({
 
 // const dateStr = 123
 const dateStr = computed(() => {
-  return new Date(props.date).toLocaleString('en-US', {
-    weekday: 'short', // long, short, narrow
-    // day: 'numeric', // numeric, 2-digit
-    year: 'numeric', // numeric, 2-digit
-    month: 'short', // numeric, 2-digit, long, short, narrow
-    hour: 'numeric', // numeric, 2-digit
-    minute: 'numeric', // numeric, 2-digit
-    second: 'numeric', // numeric, 2-digit
-  })
+  // todo: proper timezone
+  return props.date
+    ? new Date(props.date).toLocaleString('en-US', {
+      // weekday: 'short', // long, short, narrow
+      day: 'numeric', // numeric, 2-digit
+      year: 'numeric', // numeric, 2-digit
+      month: 'short', // numeric, 2-digit, long, short, narrow
+      hour: 'numeric', // numeric, 2-digit
+      minute: 'numeric', // numeric, 2-digit
+      second: 'numeric', // numeric, 2-digit
+    })
+    : ''
 })
 
 </script>
