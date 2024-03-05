397b921ee4e4225f83f0e373b6148227819f22f1
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-05 23:14:09 +0100

    style: format

--- src/components/PairItem.vue
+++ src/components/PairItem.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container class="pa-0 ma-0" style="max-width: 24em;">
-        <v-row align="center" class="pa-0 ma-0 mb-2">
-          <v-col cols="4" class="font-weight-bold pa-0 ma-0">
-            {{ capitalize(label) }}:
-          </v-col>
-          <v-col cols="8" class="pa-0 ma-0 w-auto">
-            <slot></slot>
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-row align="center" class="pa-0 ma-0 mb-2">
+      <v-col cols="4" class="font-weight-bold pa-0 ma-0">
+        {{ capitalize(label) }}:
+      </v-col>
+      <v-col cols="8" class="pa-0 ma-0 w-auto">
+        <slot></slot>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
