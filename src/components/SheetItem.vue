d5e893100383040aa1d00e9e33fbbf878f8c3e56
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-12 21:58:11 +0100

    chore: more vertical space beween SheetItems

--- src/components/SheetItem.vue
+++ src/components/SheetItem.vue
@@ -1,7 +1,7 @@
 <template>
   <v-sheet v-if="label" :elevation="2" class="px-3 py-2 font-weight-bold border-b border-darker rounded-t"
     color="grey-lighten-3">{{ capitalize(label) }}:</v-sheet>
-  <v-sheet :elevation="2" class="pa-3 mb-3">
+  <v-sheet :elevation="2" class="pa-3 mb-6">
     <v-sheet class="pt-2">
       <slot>{content}</slot>
     </v-sheet>
