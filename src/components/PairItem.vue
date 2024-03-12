4aef140c8c19140a40ddb9ea9e6abb9f448f7474
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-03-12 19:43:54 +0100

    feat(PairItem): add an optional icon before the label

--- src/components/PairItem.vue
+++ src/components/PairItem.vue
@@ -2,6 +2,7 @@
   <v-container class="pa-0 ma-0" style="max-width: 24em;">
     <v-row :align="verticalAlign" class="pa-0 ma-0 mb-2">
       <v-col cols="4" class="font-weight-bold pa-0 ma-0">
+        <v-icon v-if="prependIcon" :color="iconColor" :icon="prependIcon"></v-icon>
         {{ capitalize(label) }}:
       </v-col>
       <v-col cols="8" class="pa-0 ma-0 w-auto">
@@ -15,6 +16,8 @@
 import { capitalize, computed } from 'vue';
 const props = defineProps({
   label: String,
+  prependIcon: String, // icon name, such as "mdi-check". Just type a nonexistent icon name to create an empty space
+  iconColor: String,
   align: String   //"center" | "end" | "start" | "stretch" | "baseline"
 })
 
